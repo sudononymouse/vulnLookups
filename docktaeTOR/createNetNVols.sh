72bfa3c639cd710d0d844ce0b6f5f3a919d6adbb
JollyRogerrr <48075840+sudononymouse@users.noreply.github.com>
2025-02-21 12:07:24 -0600

    Customize .env

--- docktaeTOR/createNetNVols.sh
+++ docktaeTOR/createNetNVols.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 docker network create web
-docker network create --internal caddy_internal
+docker network create --internal docktaetor_internal
 docker volume create caddy-data
 docker volume create caddy-config
